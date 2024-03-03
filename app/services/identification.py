8a2e810d04751f40908340069d8d8411910b46a8
artemowkin <aloskan@sdvor.com>
2024-03-04 02:25:56 +0700

    Fixed lint errors

--- app/services/identification.py
+++ app/services/identification.py
@@ -10,13 +10,12 @@ ALGORITHM = "HS256"
 
 def get_token_user_id(token: str) -> int:
     try:
-        payload = jwt.decode(token, settings.secret_key,
-                                algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
         if not payload['exp'] > datetime.datetime.now(datetime.UTC).timestamp():
             raise ValueError("Incorrect token")
 
         decoded_sub = json.loads(payload['sub'])
-        if not "user_id" in decoded_sub:
+        if "user_id" not in decoded_sub:
             raise ValueError("Incorrect token")
 
         return decoded_sub['user_id']
